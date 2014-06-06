d6eccc57e2d01b90fb54232927f0efc9d3f81851
Eevee (Alex Munroe) <eevee.git@veekun.com>
2014-06-05 22:38:44 -0700

    Wrote a debug widget that intercepts stdout and stderr, thank god.

--- flax/__main__.py
+++ flax/__main__.py
@@ -21,6 +21,10 @@ PALETTE = [
 
     # Items
     ('potion', 'light magenta', 'default', None, '#f6f', 'default'),
+
+    # Debug
+    ('stdout', 'light gray', 'default', None, '#aaa', 'default'),
+    ('stderr', 'light red', 'default', None, '#d66', 'default'),
 ]
 
 class MapWidget(urwid.BoxWidget):
@@ -76,12 +80,11 @@ class CellCanvas(urwid.Canvas):
             if current_glyphs:
                 ret.append((current_attr, None, ''.join(current_glyphs).encode('utf8')))
 
-            print(ret)
             yield ret
-
 
     def content_delta(self):
         return self.content()
+
 
 class CellWidget(urwid.Widget):
     _sizing = {'box'}
@@ -106,7 +109,73 @@ class CellWidget(urwid.Widget):
         return key
 
 
-main_widget = CellWidget()
+class WriteDetectingStream:
+    def __init__(self, stream, callback):
+        self._stream = stream
+        self._pending = None
+        self._callback = callback
+
+    def __getattr__(self, attr):
+        return getattr(self._stream, attr)
+
+    def write(self, data):
+        lines = data.splitlines(True)
+        if lines:
+            if self._pending:
+                lines[0] = self._pending + lines[0]
+                self._pending = None
+            if not lines[-1].endswith('\n'):
+                self._pending = lines.pop()
+
+            for line in lines:
+                self._callback(line)
+
+        self._stream.write(data)
+
+
+class DebugWidget(urwid.ListBox):
+    def __init__(self):
+        super().__init__(urwid.SimpleListWalker([]))
+
+    def activate(self):
+        import sys
+        self.orig_stdout = sys.stdout
+        self.orig_stderr = sys.stderr
+
+        sys.stdout = WriteDetectingStream(sys.stdout, self.write_stdout)
+        sys.stderr = WriteDetectingStream(sys.stderr, self.write_stderr)
+
+    def deactivate(self):
+        import sys
+        sys.stdout = self.orig_stdout
+        sys.stderr = self.orig_stderr
+
+        del self.orig_stdout
+        del self.orig_stderr
+
+    def write_stdout(self, line):
+        self.body.append(urwid.Text(('stdout', line.rstrip('\n'))))
+        self.set_focus(len(self.body) - 1)
+
+    def write_stderr(self, line):
+        self.body.append(urwid.Text(('stderr', line.rstrip('\n'))))
+        self.set_focus(len(self.body) - 1)
+
+    def render(self, *a, **kw):
+        return super().render(*a, **kw)
+
+
+debug_widget = DebugWidget()
+main_widget = urwid.Pile([
+    CellWidget(),
+    debug_widget,
+])
 loop = urwid.MainLoop(main_widget, PALETTE)
+# TODO upstream detection for these?
+loop.screen.set_terminal_properties(
+    colors=256,
+    bright_is_bold=False,
+)
+debug_widget.activate()
 loop.run()
-#loop.screen.run_wrapper(loop._run, False)
+debug_widget.deactivate()
